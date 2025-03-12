c729de22855b982ed6153bd48929397990f968fc
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-12 13:28:14 -0700

    wip: updated the route of homepage

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const applicationsController = require('./controllers/applications.js');
+
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
@@ -32,24 +36,20 @@ app.use(passUserToView);
 
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+  if (req.session.user){
+    res.redirect(`/user/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.render('index.ejs');
   }
 });
+
 
 // control everything that needs to be sign up and sign in 
 app.use('/auth', authController);
 // then I can use the isSignedIn middleware 
 app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController);
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
