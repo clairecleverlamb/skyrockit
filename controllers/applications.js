0bdd3517a677d49db7b97462cabc64da66079730
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-12 17:02:25 -0700

    wip: needed to be edit

--- controllers/applications.js
+++ controllers/applications.js
@@ -2,16 +2,39 @@
 
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
+// GET  /users/:userId/applications
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
-    } catch(error) {
+        const currentUser = await User.findById(req.session.user._id);
+
+        res.render('applications/index.ejs', {
+            applicaitons: currentUser.applications
+        });
+    } catch (error) {
         console.log(error);
         res.redirect('/');
     }
+});
+
+// GET  /users/:userId/applications/new   
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
 })
+
+// POST /USERS/:userId/applications
+router.post('/', async (req, res) =>{
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.push(req.body);  //only save in the memory 
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
 
 module.exports = router;
